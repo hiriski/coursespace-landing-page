7eaa20cc23cac72a71bbb60e1eeebfff56e1dcad
hiriski <hiriski@outlook.com>
2022-04-24 01:55:44 +0700

    Setup eslint & prettier 🪄

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { FC } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Hello from '@/components/hello'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <Hello name="Riski dep trai :v" />
+}
+
+export default Home
